62185d2cbca5e63acee126526ba6f7ee1db55ecc
narioinc <narioinc89@gmail.com>
2020-05-17 18:10:10 +0530

    additional api workflows for register and deregsiter persistent services

--- routes/webhooks.js
+++ routes/webhooks.js
@@ -26,6 +26,7 @@ router.post("/register/dynamic", function(req, res, next) {
     }
     axios.get(webHookURL, { params })
         .then(response => {
+            console.log(response.data);
             res.json({})
         })
         .catch(err => {
@@ -36,7 +37,7 @@ router.post("/register/dynamic", function(req, res, next) {
 
 router.post("/deregister/dynamic", function(req, res, next) {
     webhookConfig = req.body
-    webHookURL = "http://BDW3vM6sTg6cKf9hfnHOwr4UCIT1q4yA@localhost:7777/api/v1/webhooks/register?"
+    webHookURL = "http://BDW3vM6sTg6cKf9hfnHOwr4UCIT1q4yA@localhost:7777/api/v1/webhooks/deregister?"
     var params = {
         "hook": webhookConfig.hook_type,
         "endpoint": webhookConfig.hook_url,
@@ -56,24 +57,42 @@ router.post("/deregister/dynamic", function(req, res, next) {
 
 router.post("/register/persistent", function(req, res, next) {
     webhookConfigs = req.body
+    duplicatesFound = false;
     webhookConfigs.forEach(webhookConfig => {
+        if (checkDuplicateWebhook(webhookConfig)) {
+            duplicatesFound = true;
+        }
         var webhook = "vmq_webhooks." + webhookConfig.hook_name + ".hook";
         var webhookEndpoint = "vmq_webhooks." + webhookConfig.hook_name + ".endpoint";
         vmqConfig[webhook] = webhookConfig.hook_type;
         vmqConfig[webhookEndpoint] = webhookConfig.hook_url;
     });
-    fs.writeFileSync('/etc/vernemq/vernemq.conf', ini.stringify(vmqConfig));
-    res.json({});
+
+    if (!duplicatesFound) {
+        fs.writeFileSync('/etc/vernemq/vernemq.conf', ini.stringify(vmqConfig));
+        return res.json({});
+    } else {
+        res.status(400);
+        return res.json({ "message": "webhook with the same combination of type and url already exists." });
+    }
+
 })
 
 
 router.post("/deregister/persistent", function(req, res, next) {
     webhookConfigs = req.body
     webhookConfigs.forEach(webhookConfig => {
-        var webhook = "vmq_webhooks." + webhookConfig.hook_name + ".hook";
-        var webhookEndpoint = "vmq_webhooks." + webhookConfig.hook_name + ".endpoint";
-        delete vmqConfig[webhook]
-        delete vmqConfig[webhookEndpoint]
+        for (let [key, val] of Object.entries(vmqConfig)) {
+            if (key.indexOf('vmq_webhooks') == 0 && key.indexOf(".hook") == (key.length - 5)) {
+                url_key = key.replace(".hook", ".endpoint")
+                if (vmqConfig[key] == webhookConfig.hook_type && vmqConfig[url_key] == webhookConfig.hook_url) {
+                    console.log("gfot the key");
+                    delete vmqConfig[key]
+                    delete vmqConfig[url_key]
+                }
+            } else continue;
+        }
+
     });
     fs.writeFileSync('/etc/vernemq/vernemq.conf', ini.stringify(vmqConfig));
     res.json({});
@@ -86,7 +105,7 @@ router.post("/enable", function(req, res, next) {
 })
 
 router.post("/disable", function(req, res, next) {
-    config.plugins.vmq_webhooks = "off";
+    vmqConfig.plugins.vmq_webhooks = "off";
     fs.writeFileSync('/etc/vernemq/vernemq.conf', ini.stringify(vmqConfig));
     res.json({});
 })
@@ -100,5 +119,17 @@ router.post("/test2", function(req, res, next) {
     console.log(req.body)
     res.json({})
 })
+
+checkDuplicateWebhook = function(webhookConfig) {
+    for (let [key, val] of Object.entries(vmqConfig)) {
+        if (key.indexOf('vmq_webhooks') == 0 && key.indexOf(".hook") == (key.length - 5)) {
+            url_key = key.replace(".hook", ".endpoint")
+            if (vmqConfig[key] == webhookConfig.hook_type && vmqConfig[url_key] == webhookConfig.hook_url) {
+                return true
+            }
+        } else continue;
+    }
+    return false;
+}
 
 module.exports = router;
